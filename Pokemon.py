31ddcfc1c47d218a09d344000bfd4df4cd08d60e
AidenCohen <acohen@sjnma.org>
2017-07-06 15:45:32 -0500

    cool

---
+++ Pokemon.py
@@ -0,0 +1,16 @@
+print("Hello there! Welcome to the world of POKEMON! " +
+" My name is OAK!" +  " People call me the POKEMON PROF!")
+print( "This world is inhabited by creatures called POKEMON!" +
+" For some people, POKEMON are pets. Others use them for fights." +
+" Myself... I study POKEMON as a profession.")
+name = input("First, what is your name?")
+print( "Right! So your name is " + name)
+print("This is my grandson. He's been your rival since you were a baby.")
+enemyName = input( "...Erm, what is his name again?")
+print("That's right! I remember now! His name is " + name)
+print(name + " " + "Your very own POKEMON legend is about to unfold!" +
+"A world of dreams and adventures with POKEMON awaits! Let's go!")
+
+def print1():
+
+    
