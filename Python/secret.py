91dae9c3511c8fa6be91f35879050c37dda2fb9b
AidenCohen <acohen@sjnma.org>
2017-07-18 12:54:57 -0500

    Update secret.py

--- Python/secret.py
+++ Python/secret.py
@@ -3,13 +3,13 @@ import smtplib
 
 sender = 'acohen@sjnma.org'
 receivers = ['dgold@sjnma.org']
-random1  = ["a","b","c","d","e","f","g","h","i","j","k","l", "o" ,"m", "p" ,"q","r","s", "t" ,"u" , "v" ,"w", "x", "y", "z"]
+random1  = ["a","b","c","d","e","f","g","h","i","j","k","l", "o" ,"m", "p" ,"q","r","s", "t" ,"u" , "v" ,"w", "x", "y", "z",1,2,3,4,5,6,7,8,9,0]
 def aiden:
-    for x in range(5):
-        random.randint(0,26)
+    for x in range(11):
+        random.randint(0,35)
         y = []
         y.append(x)
-
+random1 = []
 message = """From: From Person <????>
 To: To David Gold <dgold@sjnma.org>
 Subject: This is an email bot
@@ -25,9 +25,10 @@ try:
         smtpObj.sendmail(sender, receivers, message)
     while True
         time.sleep(172800)
-        while random 
+        while random1
         random 2 =
         sender = random2 + '@gmail.com'
    print "Successfully sent email"
 except SMTPException:
    print "Error: unable to send email"
+   
